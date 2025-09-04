69715e7399333839bfdfbf572147492aa0f1028c
Nguyen Anh Tuan <22521604@gm.uit.edu.vn>
2025-09-04 18:21:50 +0700

    complete OrderItemModel and OrderModel

--- order-service/src/main/java/order_service/order/model/OrderModel.java
+++ order-service/src/main/java/order_service/order/model/OrderModel.java
@@ -4,11 +4,16 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -18,6 +23,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "orders")
+@Builder
 public class OrderModel {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -26,10 +32,20 @@ public class OrderModel {
     @Column(nullable = false)
     private String userId;
 
-    // private List<T> listofProduct;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private List<OrderItemModel> listofItemModel;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
